efaa082d9b92704d6d05fdaa9765b34fdcc48b79
TN Botz <163137175+sakthiraj2008@users.noreply.github.com>
2025-02-26 03:30:53 -0800

    Update vars.py

--- WOODcraft/vars.py
+++ WOODcraft/vars.py
@@ -10,17 +10,17 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('11472991'))
-    API_HASH = str(getenv('c78c50d54baf2173e8b3f75c359c0c72'))
-    BOT_TOKEN = str(getenv('7754306060:AAGBIJsYxqYe9vUPKuCsg5t7i5xS786ufWk'))
-    name = str(getenv('name', 'filetolinkbot'))
-    SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
-    WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('-1002262171720'))
-    PORT = int(getenv('PORT', 8080))
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
-    PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())  
+    API_ID = int(('11472991'))
+    API_HASH = str(("c78c50d54baf2173e8b3f75c359c0c72"))
+    BOT_TOKEN = str(('7754306060:AAGBIJsYxqYe9vUPKuCsg5t7i5xS786ufWk'))
+    name = str(('name', 'filetolinkbot'))
+    SLEEP_THRESHOLD = int(('SLEEP_THRESHOLD', '60'))
+    WORKERS = int(('WORKERS', '4'))
+    BIN_CHANNEL = int(('-1002262171720'))
+    PORT = int(('PORT', 8080))
+    BIND_ADRESS = str(('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
+    PING_INTERVAL = int(("PING_INTERVAL", "1200"))  # 20 minutes
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1430742022").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = str(getenv('APP_NAME'))
     OWNER_USERNAME = str(getenv('coder_kakashi_bot'))
